a31f32b9912c0435496ca4b1d4c9f71837d8b4fa
AshithGannarapu16 <ashithgannarapu@gmail.com>
2024-08-07 10:59:27 +0530

    array

---
+++ Inception/array.js
@@ -0,0 +1,32 @@
+// let books=["java","python","c++","c"];
+// console.log(books[1])
+
+// let books=["java","python","JSON"];
+// books[0]="javascript"
+
+
+// console.log(books)
+// console.log(typeof books)
+
+//push
+// let languages=["java","python","c++","c"];
+// languages.push("javascript");
+// console.log
+
+//pop
+// let items=["idly","dosa","chapathi","bonda"];
+// console.log(items)
+// let removed=items.pop();
+// console.log(removed)
+
+
+//unshift
+
+// let items=["idly","dosa","puri"];
+// items.unshift("pulihora");
+// console.log
+
+//shift
+let items=["idly","puri","dosa","vada"]
+items.shift();
+console.log(items)
