2f90c8c71b4ca06c90fe6aa033cdb6edd5a43973
AshithGannarapu16 <ashithgannarapu@gmail.com>
2024-09-20 13:06:49 +0530

    changes

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+        import React from "react";
+        import ReactDOM from "react-dom/client";  
+        
+        
         const heading=React.createElement("h1",{id:"heading"},"Hello world from react.js");
         const root=ReactDOM.createRoot(document.getElementById("root"));
         root.render(heading);
@@ -28,7 +32,7 @@
                     "div",{
                         id:"child"
                     },
-                    [React.createElement("h1",{},"I'm an h1 tag"),React.createElement("h2",{},"I'm an h2 tag")]
+                    [React.createElement("h1",{},"This is namaste-reactj"),React.createElement("h2",{},"I'm an h2 tag")]
                 ),React.createElement(
                     "div",{
                         id:"child2"
